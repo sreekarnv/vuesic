d5edb52031d62b7e221f2fe8638158d680d203ed
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2022-02-16 14:18:20 +0530

    update home page styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="container d-flex flex-column justify-content-center align-items-center pt-4">
-    <h1 class="mb-4">Home Page</h1>
+    <h1 class="fw-bold text-dark ">Vuesic</h1>
+    <p class="mb-4">Built with
+      <a
+        href="https://vuejs.org/"
+        class="text-primary"
+      >
+        Vue Js
+      </a>
+      and
+      <a
+        class="text-primary"
+        href="https://firebase.google.com/"
+      >
+        Firebase
+      </a>
+    </p>
     <button
       @click="loginUser"
-      class="btn btn-gradient"
+      class="btn btn-gradient mb-5"
     >Log in / Sign Up</button>
+    <img
+      class="img-fluid home-bg-image"
+      src="../assets/home.jpg"
+      alt="home-bg"
+    >
   </div>
 </template>
 
@@ -24,3 +44,13 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="scss" scoped>
+.home-bg-image {
+  height: 550px;
+
+  @media screen and (max-width: 768px) {
+    height: 400px;
+  }
+}
+</style>
